d5b938c373f84ea22f6deef4054514dee845841b
ibrohimov-murodjon <murodjon.ibrohimov.rishton@gmail.com>
2023-12-19 23:02:18 +0500

    hero changed

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -43,22 +43,26 @@ export default function Hero() {
             </Slide>
           </div>
         </SwiperSlide>
-        {/* <SwiperSlide className="slider3 hero-swiper z-[12]">
+        <SwiperSlide className="slider3 hero-swiper z-[12]">
           <div className="flex mx-auto w-full max-w-[1246px] px-[20px] flex-col">
             <h1 className="flex flex-wrap leading-[70px] items-center  text-[56px] font-bold text-white w-full italic max-w-[650px] mb-2 mobileMax:text-[50px] mobileMax:leading-[60px] mobileLg:text-[40px] mobileLg:leading-[45px] mobileMd:text-[30px] mobileMd:leading-[35px]">
-              RUNNING ON THE POWER OF
-              <span className="text-[#ED3237] pr-4 underline underline-offset-4">
-                PROMISES
-              </span>
-              KEPT
+              <Fade cascade damping={1} delay={1800}>
+                <span>RUNNING ON THE POWER OF</span>
+                <span className="text-[#ED3237] pr-4 underline underline-offset-4">
+                  PROMISES
+                </span>
+                <span>KEPT</span>
+              </Fade>
             </h1>
-            <p className="text-white w-full max-w-[650px] mb-4 mobileLg:text-[15px] mobileLg:mt-4 mobileLg:max-w-[400px]">
-              Our priority relies on ensuring your packages reach their
-              destination in record time, bringing you unparalleled speed and
-              reliability.
-            </p>
+            <Slide direction="left" duration={2500} delay={2000}>
+              <p className="text-white w-full max-w-[650px] mb-4 mobileLg:text-[15px] mobileLg:mt-4 mobileLg:max-w-[400px]">
+                Our priority relies on ensuring your packages reach their
+                destination in record time, bringing you unparalleled speed and
+                reliability.
+              </p>
+            </Slide>
           </div>
-        </SwiperSlide> */}
+        </SwiperSlide>
       </Swiper>
     </div>
   );
